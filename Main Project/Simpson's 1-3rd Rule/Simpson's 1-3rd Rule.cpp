53545dd0b2edec3dc29e6c3d5b62c01884110e0c
Ariful Islam <iariful2002@gmail.com>
2025-12-14 19:09:33 +0600

    Simpson's 1-3rd Rule added

---
+++ Main Project/Simpson's 1-3rd Rule/Simpson's 1-3rd Rule.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double func(double x){
+    return 1/(1+x*x);
+}
+
+double simpson13(double a, double b, int n){
+    double h = (b-a) / n;
+    double x[n+1], y[n+1];
+
+    for(int i=0; i<n+1; i++){
+        x[i] = a + i*h;
+        y[i] = func(x[i]);
+    }
+
+    double ans = 0;
+    for(int i=0; i<n+1; i++){
+        if(i==0 || i==n){
+            ans += y[i];
+        }
+        else if(i%2){
+            ans += 4*y[i];
+        }
+        else{
+            ans += 2*y[i];
+        }
+    }
+    ans = ans*(h/3);
+    return ans;
+}
+
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    if(!fin){
+        cout<<"Error: input.txt not found!"<<endl;
+        return 0;
+    }
+    if(!fout){
+        cout<<"Error: Can't open output.txt!"<<endl;
+        return 0;
+    }
+
+    double a, b;
+    int n;
+    fin>>a>>b>>n;
+
+    fout<<"\nf(x) = 1 / (1 + x * x)\n"<<"where, lower limit = "<<a<<" , upper limit = "<<b<<" , Number of intervals = "<<n<<endl;
+    fout<<"\nThe value of integral: "<<simpson13(a, b, n)<<endl;
+
+    return 0;
+}
